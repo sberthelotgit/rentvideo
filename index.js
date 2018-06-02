3c0c3cb26015fa88a01f086a426e5a3b949369f5
sberthelot <sberthelot@legal-suite.com>
2018-06-02 15:02:11 -0400

    Adding put support

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(express.json());
 
 const apiUrl = '/api/movies/';
 
+// Get home
+app.get('/', (req, res) => {
+    res.send(`Welcome to movie rental website<br/> 
+        Use following path to access the api : ${apiUrl}`);
+});
 
 // Get all movie
 app.get(apiUrl, (req, res) => {
@@ -22,10 +27,9 @@ app.get(apiUrl, (req, res) => {
 
 // Get movie by ID
 app.get(apiUrl + ':id', (req, res) => {
-    const predicate = c => {
-       return c.id === parseInt(req.params.id);
-    };
-    const movie = listMovies.find(predicate);
+    const movie = listMovies.find(c => {
+        return c.id === parseInt(req.params.id);
+    });
     if (!movie) {
         return res.status(404).send(`Movie with id ${req.params.id} not found`);
     }
@@ -49,6 +53,27 @@ app.post(apiUrl, (req, res) => {
     res.send(newMovie);
 });
 
+// Update movie by ID
+app.put(apiUrl + ':id', (req, res) => {
+    const movie = listMovies.find(c => {
+        return c.id === parseInt(req.params.id);
+    });
+    if (!movie) {
+        return res.status(404).send(`Movie with id ${req.params.id} not found`);
+    }
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        return res.status(400).send(result.error.message);
+    }
+    if (req.body.name) {
+        movie.name = req.body.name;
+    }
+    if (req.body.genre) {
+        movie.genre = req.body.genre;
+    }
+    res.send(movie);
+});
+
 
 // Starting listening
 app.listen(4000, () => {
@@ -58,4 +83,5 @@ app.listen(4000, () => {
 // Schema for post and put validation
 const schema = {
     name: Joi.string().min(3).required(),
+    genre: Joi.string(),
 };
