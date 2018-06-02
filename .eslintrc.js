f6f4f01abca8fa3e30b3983edeaec4aca57e9b0a
sberthelot <sberthelot@legal-suite.com>
2018-06-02 00:43:31 -0400

    Commit Init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = {
+    "extends": ["google"],
+    "parserOptions": {
+        "ecmaVersion": 6,
+    },
+    "rules": {
+        "eol-last": "off",
+        "arrow-parens": ["error","as-needed"],
+    }
+};
