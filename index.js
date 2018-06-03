3222f1bb8c577dbd2bfda407392f9e47f711c71b
sberthelot <sberthelot@legal-suite.com>
2018-06-03 00:00:14 -0400

    Trying custom middleware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const movies = require('./routes/movies');
 const home = require('./routes/home');
+const logger = require('./middleware/logger');
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 app.use('/api/movies', movies);
 app.use('/', home);
 
